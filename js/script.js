164e7c3e218c290484d9b7c2a88689f0713c6e9c
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-11-28 14:45:31 -0500

    umm math

--- js/script.js
+++ js/script.js
@@ -26,7 +26,12 @@ function calculate() {
   const c = parseInt(document.getElementById("length-c").value)
 
   // process
-  const math = a + b + c
+  const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
+  const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
+  const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+
+  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2)) 
+  Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
   // output
   document.getElementById("words").innerHTML = "Numbers: " + math + " cm"
 }
